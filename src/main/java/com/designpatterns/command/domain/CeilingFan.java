594b49436c0061d7f75d5a328a94a43c3a9ada7b
vneplokh <neplokh.v@Gmail.com>
2019-03-11 19:04:56 +0200

    command pattern added

--- src/main/java/com/designpatterns/command/domain/CeilingFan.java
+++ src/main/java/com/designpatterns/command/domain/CeilingFan.java
@@ -1,10 +1,20 @@
 package com.designpatterns.command.domain;
 
 public class CeilingFan {
+	private Speed speed;
+
+	public CeilingFan() {
+		this.speed = Speed.SLOW;
+	}
+
 	public void on() {
 		System.out.println("Ceiling fan is on");
 	}
 	public void off() {
 		System.out.println("Ceiling fan is off");
+	}
+
+	enum Speed {
+		FAST, MEDIUM, SLOW;
 	}
 }
